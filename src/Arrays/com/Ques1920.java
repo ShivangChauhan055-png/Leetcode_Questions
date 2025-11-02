08a877625b7b5fa1b53d88caf473fdc4aed3cae0
Shivang Chauhan <shivangchauhan055@gmail.com>
2025-11-02 17:03:39 +0530

    Added Array problems Ques1920 and Ques1929

---
+++ src/Arrays/com/Ques1920.java
@@ -0,0 +1,29 @@
+package Arrays.com;
+import java.util.*;
+
+
+public class Ques1920 {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Ques1920 obj = new Ques1920();
+        int[] nums = {0, 2, 1, 5, 3, 4};
+        int[] result = obj.buildArray(nums);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+        /*
+         Approach 1 → Using O(n)
+Initialize a different array temp and simply implement the condition asked in the problem statement
+directly for every index in `ans`.
+         */
+
+
+
---
+++ src/Arrays/com/Ques1929.java
@@ -0,0 +1,22 @@
+package Arrays.com;
+import java.util.*;
+
+public class Ques1929 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1}; // Example input
+        int[] result = getConcatenation(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+
+    }
+}
