890da6d33cac6abddd24c7b71f34d59d4fd49833
Shivang Chauhan <shivangchauhan055@gmail.com>
2025-11-07 18:04:00 +0530

    Added solution for Leetcode Ques1295

--- src/Arrays/Ques1295.java
+++ src/Arrays/Ques1295.java
@@ -1,4 +1,27 @@
 package Arrays;
 
 public class Ques1295 {
+    public static void main(String[] args) {
+        int[] nums = {12, 345, 2, 6, 7896};
+
+        int count = 0;
+
+        for (int n : nums) {
+            int digit = 0;
+            int x = n;
+
+            // Count digits
+            while (x > 0) {
+                digit++;
+                x /= 10;
+            }
+
+            // Check even digit
+            if (digit % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Count of numbers with even digits: " + count);
+    }
 }
