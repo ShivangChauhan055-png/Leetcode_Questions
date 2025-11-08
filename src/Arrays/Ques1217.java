b29b8191c1c1c768d09cba6f4d8a8e416e12bfc8
Shivang Chauhan <shivangchauhan055@gmail.com>
2025-11-08 16:18:20 +0530

    Added solution for Leetcode Ques1217

--- src/Arrays/Ques1217.java
+++ src/Arrays/Ques1217.java
@@ -1,4 +1,22 @@
 package Arrays;
 
 public class Ques1217 {
+    public static void main(String[] args) {
+        int[] position = {2,2, 2, 3,3};
+
+        int result = minCostToMoveChips(position);
+        System.out.println(result);
+    }
+
+    public static int minCostToMoveChips(int[] position) {
+        int even = 0, odd = 0;
+
+        // Count how many chips are on even and odd positions
+        for (int n : position) {
+            if (n % 2 == 0) even++;
+            else odd++;
+        }
+        // Minimum cost = move the smaller group to the other side
+        return Math.min(even, odd);
+    }
 }
