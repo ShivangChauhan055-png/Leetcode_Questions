1d349d4f3df8e47feec384294e703572c89af1bc
Shivang Chauhan <shivangchauhan055@gmail.com>
2025-11-17 23:25:38 +0530

    solution

--- src/Math/Palindrome.java
+++ src/Math/Palindrome.java
@@ -1,4 +1,22 @@
 package Math;
 
+import java.util.Scanner;
+
 public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int original=n;
+        int number =0;
+        while (n!=0){
+            int rem = n%10;
+            number= number*10+rem;
+            n/=10;
+        }
+        if(number==original){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+    }
 }
