49ddee28f480981a8371a699f7434267db458bec
Shivang Chauhan <shivangchauhan055@gmail.com>
2025-11-18 15:24:40 +0530

    Added solution for Leetcode Ques1134

--- src/Math/Ques1134.java
+++ src/Math/Ques1134.java
@@ -1,4 +1,24 @@
 package Math;
 
+import java.util.Scanner;
+
 public class Ques1134 {
+    public static void main(String[] args) {
+        // find armstrong number --> sum of cubes of its digits equal to the number itself
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int original = n;
+        int sum=0;
+        while(n>0){
+            int rem = n%10;
+            sum=sum+(rem*rem*rem);
+            n/=10;
+        }
+        if(sum==original){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+    }
 }
