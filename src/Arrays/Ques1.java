93d4b5de34d23b191cb2ab418b752cca633ec565
Shivang Chauhan <shivangchauhan055@gmail.com>
2026-02-03 22:54:13 +0530

    ques1

--- src/Arrays/Ques1.java
+++ src/Arrays/Ques1.java
@@ -1,6 +1,7 @@
 package Arrays;
 
 import java.util.*;
+import java.util.HashMap;
 public class Ques1 {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
@@ -11,14 +12,28 @@ public class Ques1 {
     }
 
     // Brute-force solution (without HashMap)
+    // tle - O(n*n)
     public static int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return new int[]{};
+
+        // using hashMap
+        HashMap<Integer,Integer> m = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            int c  = target-nums[i];
+            if(m.containsKey(c)){
+                return new int[]{i,m.get(c)};
+            }else{
+                m.put(nums[i],i);
             }
         }
-        return new int[]{};
+        return new int[]{-1,-1};
+
     }
 }
