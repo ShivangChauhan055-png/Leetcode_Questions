8c293018aae6eadc68623c232e45ff39092d1a23
Shivang Chauhan <shivangchauhan055@gmail.com>
2026-04-13 11:46:50 +0530

    Ques1848

---
+++ src/Arrays/Ques1848.java
@@ -0,0 +1,13 @@
+package Arrays;
+class Ques1848 {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                ans = Math.min(ans,Math.abs(i-start));
+            }
+        }
+        return ans;
+    }
+}
